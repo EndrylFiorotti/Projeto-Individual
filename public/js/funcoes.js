990c3388e401703f708132117d2d0cdbff3d4c10
EndrylFiorotti <endryl.fiorotti@sptech.school>
2022-05-18 21:11:19 -0300

    Atualização 18/05

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -22,28 +22,40 @@ function validarSessao() {
     }
 }
 
-function validarLivros() {
-    // aguardar();
-    var nomeLivroV = sessionStorage.NOME_LIVRO;
-    var nomeSinopseV = sessionStorage.SINOPSE_LIVRO;
-    var nomeImgV = sessionStorage.IMG_LIVRO;
-    var idSessao = sessionStorage.ID_SESSAO;
+// function validarLivros() {
+//     // aguardar();
 
-    var h1LoginUsuario = document.getElementById("h1_login_usuario");
+//     fetch("/livros/listar", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//             // crie um atributo que recebe o valor recuperado aqui
+//             // Agora vá para o arquivo routes/usuario.js
+//             nomeServer: nomeVar
+//         })
+//     }).then(function (resposta) {
 
-    if (email != null && nome != null) {
-        // window.alert(`Seja bem-vindo, ${nome}!`);
-        if (h1LoginUsuario != undefined) {
-            h1LoginUsuario.innerHTML = email;
-        }
-        b_usuario.innerHTML = nome;
-        b_id_sessao.innerHTML = idSessao;
+//         console.log("resposta: ", resposta);
 
-        // finalizarAguardar();
-    } else {
-        window.location = "../login.html";
-    }
-}
+//         if (resposta.ok) {
+            
+//         } else {
+//             throw ("Houve um erro ao tentar realizar a busca!");
+//         }
+//     }).catch(function (resposta) {
+//         console.log(`#ERRO: ${resposta}`);
+//         // finalizarAguardar();
+//     });
+
+//     var nomeLivroV = NOME_USUARIO;
+//     // var idSessao = sessionStorage.ID_SESSAO;
+
+//         caixa_livros.innerHTML += nomeLivroV;
+//         caixa_livros.innerHTML += sinopseV;
+//         caixa_livros.innerHTML += imgV;
+// }
 
 function limparSessao() {
     // aguardar();
